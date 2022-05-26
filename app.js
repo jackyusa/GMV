8de657bc17cc6aad7899a68249fc8c77917eda7e
Jacky Li <90122922+jackyusa@users.noreply.github.com>
2022-05-26 17:57:57 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cursor = document.querySelector('.cursor');
 window.addEventListener('mousemove', (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
-    cursor.setAttribute('data-fromTop', (cursor.offSetTop - scrollY))
+    cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY))
 });
 
 window.addEventListener('scroll', () => {
