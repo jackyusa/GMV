f16bd6946291a2aa73bc1f751136a6b60aee7c99
Jacky Li <90122922+jackyusa@users.noreply.github.com>
2022-06-08 17:09:24 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
 const logo = document.querySelector('.logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
 const para = document.querySelector('.para');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', ()=>{
+    menu.classList.toggle('open');
+});
+
+menu.addEventListener('click', ()=>{
+    menu.classList.toggle('open');
+});
 
 const t1 = new TimelineMax();
 
 t1.fromTo(hero,1,{height: "220%"},{height: '100%'})
 .fromTo(para,.8,{x:'-110%'},{x:'0%'})
 .fromTo(logo,.8,{y:'-400%'},{y:'0%'},"-=.8")
-.fromTo(hamburger,.8,{y:'-400%'},{y:'0%'},"-=.7")
+.fromTo(hamburger,.8,{y:'-400%'},{y:'0%'},"-=.65")
 .fromTo(hero,.8,{width: '100%'},{width:'96%'},"-=.8");
 
 var i = 0;
@@ -18,7 +26,6 @@ var txt = 'Green Market Ventures';
 var speed = 50;
 var x = 0;
 var checker = 0;
-
 
 window.onloadstart = wait();
 
@@ -33,8 +40,3 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 };
-
-AOS.init({
-    offset:400,
-    duration: 1000
-});
